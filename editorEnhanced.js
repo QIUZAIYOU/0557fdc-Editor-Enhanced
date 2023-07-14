6c7a00f43ae1c203dcb2b64b0bdac7ada7bd0bdb
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2023-07-14 11:21:44 +0800

    Update editorEnhanced.js

--- editorEnhanced.js
+++ editorEnhanced.js
@@ -2,7 +2,7 @@
 // @name         宿房网后台新闻编辑器功能增强
 // @license      GPL-3.0 License
 // @namespace    https://github.com/QIUZAIYOU/0557FDC-EditorEnhanced
-// @version      0.22
+// @version      0.23
 // @description  宿房网后台新闻编辑器功能增强,自动优化标题及描述,扩展排版功能
 // @author       QIAN
 // @match        https://www.0557fdc.com/admin/*
@@ -125,7 +125,7 @@
   function convertStringToArrayAndRemoveDuplicates(str, addonStr) {
     const moreBlankRegex = /\s+/g;
     const noneNecessarySymbol = /\s|，|、/g;
-    const fullStr = `${str.replace(moreBlankRegex, ' ').replace(noneNecessarySymbol, ',')},${addonStr}`;
+    const fullStr = `${str.replace(moreBlankRegex, ' ').replace(noneNecessarySymbol, ',')}${str ? ',' : ''}${addonStr}`;
     const arr = fullStr.replace(/,+/g, ',').split(',');
     const uniqueSet = new Set(arr);
     const newStr = Array.from(uniqueSet).join(',');
@@ -146,15 +146,15 @@
     const handleImageStyleButton = createButton('handleImageStyleButton', '图片处理', '图片处理(仅适用于简单排版)', '<path fill="#222f3e" d="M368 480c-62.4 0-112-49.6-112-112s49.6-112 112-112 112 49.6 112 112-49.6 112-112 112zm0-160c-27.2 0-48 20.8-48 48s20.8 48 48 48 48-20.8 48-48-20.8-48-48-48zm464 608H192c-52.8 0-96-43.2-96-96V192c0-52.8 43.2-96 96-96h640c52.8 0 96 43.2 96 96v640c0 52.8-43.2 96-96 96zM192 160c-17.6 0-32 14.4-32 32v640c0 17.6 14.4 32 32 32h640c17.6 0 32-14.4 32-32V192c0-17.6-14.4-32-32-32H192zm259.2 556.8c-25.6 0-51.2-11.2-70.4-30.4l-38.4-40c-12.8-12.8-33.6-12.8-46.4 0l-49.6 52.8c-12.8 12.8-32 12.8-44.8 1.6s-12.8-32-1.6-44.8l49.6-52.8c17.6-19.2 43.2-30.4 68.8-30.4s51.2 11.2 70.4 30.4l38.4 40c12.8 12.8 33.6 12.8 46.4 0l160-168c17.6-19.2 43.2-30.4 70.4-30.4s51.2 11.2 70.4 30.4L920 628.8c12.8 12.8 11.2 33.6-1.6 44.8-12.8 12.8-33.6 11.2-44.8-1.6L728 518.4c-12.8-12.8-33.6-12.8-46.4 0L521.6 688c-19.2 17.6-44.8 28.8-70.4 28.8z"/>');
     const insertBlankElementButton = createButton('insertBlankElementButton', '插入空段', '插入空段', '<path fill="#222f3e" d="M871.32 559.104c35.674 0 64.594 28.71 64.594 64.138v248.534c0 35.422-28.92 64.138-64.595 64.138H152.681c-35.675 0-64.595-28.716-64.595-64.138V623.242c0-35.427 28.92-64.138 64.595-64.138h718.638zm3.638 60.95H154.58V869.42h720.378V620.054zm-523.66 72.038a52.642 52.642 0 1 1 0 105.29 52.642 52.642 0 0 1 0-105.29zm160.702 0a52.642 52.642 0 1 1 0 105.29 52.642 52.642 0 0 1 0-105.29zm160.702 0a52.642 52.642 0 1 1 0 105.29 52.642 52.642 0 0 1 0-105.29zM871.319 88.086c35.675 0 64.595 28.942 64.595 64.65v258.598c0 35.703-28.92 64.65-64.595 64.65H152.681c-35.675 0-64.595-28.947-64.595-64.65V152.736c0-35.708 28.92-64.65 64.595-64.65h718.638zm3.64 60.956H154.58v260.442h720.378V149.042zm-523.66 77.576a52.642 52.642 0 1 1 0 105.29 52.642 52.642 0 0 1 0-105.29zm160.701 0a52.642 52.642 0 1 1 0 105.29 52.642 52.642 0 0 1 0-105.29zm160.702 0a52.642 52.642 0 1 1 0 105.29 52.642 52.642 0 0 1 0-105.29z" data-spm-anchor-id="a313x.7781069.0.i13"/>');
     const adjustLineHeightButton = createButton('adjustLineHeightButton', '调整行高', '调整行高', '<path fill="#222f3e" d="M256 298.667v170.666h-85.333V298.667h-128L213.333 128 384 298.667H256zm213.39-128h426.667V256H469.39v-85.333zm426.667 298.666h-512v85.334h512v-85.334zM384 725.333H256V554.667h-85.333v170.666h-128L213.333 896 384 725.333zM469.39 768h426.667v85.333H469.39V768z" class="selected" data-spm-anchor-id="a313x.7781069.0.i14"/>');
-
     const addImageAlternativeDescriptionButton = createButton('addImageAlternativeDescriptionButton', '图片添加Alt', '图片添加Alt(先在编辑器里添加Alt后再使用此功能)', '<path d="M9 8.81a3.67 3.67 0 0 1-.15.63l-1.4 3.78h3.05L9.13 9.44A3.42 3.42 0 0 1 9 8.81z" fill="none"/><path d="M19.5 3.75h-15a.76.76 0 0 0-.75.75v15a.76.76 0 0 0 .75.75h15a.76.76 0 0 0 .75-.75v-15a.76.76 0 0 0-.75-.75zm-7.71 13l-.93-2.49H7.08l-.87 2.49h-1.3l3.46-9.1h1.26l3.45 9.08zm2.92 0h-1.15V7.13h1.15zm4.38-5.54h-1.62v3.54a1.55 1.55 0 0 0 .22.92.89.89 0 0 0 .72.27 1.14 1.14 0 0 0 .68-.21v.95a2.09 2.09 0 0 1-1 .21c-1.16 0-1.74-.65-1.74-1.93v-3.77h-1.13v-.94h1.11V8.69l1.14-.37v1.93h1.62z" fill="none"/><path d="M19.5 2.25h-15A2.25 2.25 0 0 0 2.25 4.5v15a2.25 2.25 0 0 0 2.25 2.25h15a2.25 2.25 0 0 0 2.25-2.25v-15a2.25 2.25 0 0 0-2.25-2.25zm.75 17.25a.76.76 0 0 1-.75.75h-15a.76.76 0 0 1-.75-.75v-15a.76.76 0 0 1 .75-.75h15a.76.76 0 0 1 .75.75z" fill="#222f3e"/><path d="M8.37 7.65l-3.46 9.08h1.3l.87-2.49h3.78l.93 2.49h1.29L9.63 7.65zm-.92 5.57l1.36-3.78A3.67 3.67 0 0 0 9 8.81a3.42 3.42 0 0 0 .14.63l1.37 3.78zM13.56 7.13h1.15v9.59h-1.15zM17.47 8.32l-1.14.37v1.56h-1.11v.94h1.11v3.75c0 1.28.58 1.93 1.74 1.93a2.09 2.09 0 0 0 1-.21v-.95a1.14 1.14 0 0 1-.68.21.89.89 0 0 1-.72-.27 1.55 1.55 0 0 1-.22-.92v-3.54h1.62v-.94h-1.6z" fill="#222f3e"/>', '0 0 24 24');
-
+    const handelTableStyleButton = createButton('handelTableStyleButton', '处理表格', '处理表格', '<path fill="#222f3e" d="M959.825 384.002V191.94c0-70.692-57.308-128-128-128H191.94c-70.692 0-128 57.308-128 128v639.885c0 70.692 57.308 128 128 128h639.885c70.692 0 128-57.308 128-128V384.002zm-813.16-237.337a63.738 63.738 0 0 1 45.336-18.785H832a63.962 63.962 0 0 1 63.886 64.121v128.061H127.88v-128.06a63.738 63.738 0 0 1 18.785-45.337zm269.127 461.308v-223.97h192.181v223.97H415.792zm192.181 63.94v223.972H415.792V671.914h192.181zm-256.121-63.94H127.88v-223.97h223.972v223.97zM146.665 877.21a63.467 63.467 0 0 1-18.785-45.21V671.914h223.972v223.97h-159.85a63.626 63.626 0 0 1-45.337-18.675zm749.22-45.21a63.763 63.763 0 0 1-63.886 63.886H671.914V671.914h223.97v160.085zm0-224.026H671.914v-223.97h223.97v223.97z"/>', undefined, 22, 22);
     appendHTMLString('.tox-dialog__footer-start', removeAllEmptyParagraphsButton);
     appendHTMLString('.tox-dialog__footer-start', removeBackgroundButton);
     appendHTMLString('.tox-dialog__footer-start', handleImageStyleButton);
     appendHTMLString('.tox-dialog__footer-start', insertBlankElementButton);
     appendHTMLString('.tox-dialog__footer-start', adjustLineHeightButton);
     appendHTMLString('.tox-dialog__footer-start', addImageAlternativeDescriptionButton);
+    appendHTMLString('.tox-dialog__footer-start', handelTableStyleButton);
 
     document.getElementById('removeAllEmptyParagraphsButton').addEventListener('click', () => {
       const domElement = htmlToNode(newsTextarea.value);
@@ -186,6 +186,11 @@
       const result = addImageAlternativeDescription(domElement);
       newsTextarea.value = result.innerHTML;
     });
+    document.getElementById('handelTableStyleButton').addEventListener('click', () => {
+      const domElement = htmlToNode(newsTextarea.value);
+      const result = handelTableStyleIssues(domElement);
+      newsTextarea.value = result.innerHTML;
+    });
   }
 
   function formtNewsInformation(parentSelector) {
@@ -210,7 +215,6 @@
     const editor_iframe = parent.querySelector('.tox-edit-area>iframe').contentWindow.document.querySelector('#tinymce');
     const thumb = parent.querySelector('.el-image__inner');
     const year = parent.querySelector('div.el-date-editor.el-input.el-input--small.el-input--prefix.el-input--suffix.el-date-editor--datetime > input').value.trim().slice(0, 4);
-
     // const addImageAlternativeDescriptionDom = addImageAlternativeDescription(htmlToNode(editor_iframe.innerHTML));
     // editor_iframe.innerHTML = addImageAlternativeDescriptionDom.innerHTML
 
@@ -401,6 +405,27 @@
       let imgAlt = currentImg.alt;
       const addonAlt = '宿房网出品,宿州楼市动态尽在掌握';
       currentImg.alt = imgAlt ? convertStringToArrayAndRemoveDuplicates(`新闻配图${index},${imgAlt}`, addonAlt) : convertStringToArrayAndRemoveDuplicates(`新闻配图${index}`, addonAlt);
+    }
+    return cloneDom;
+  }
+  function handelTableStyleIssues(dom) {
+    const cloneDom = dom.cloneNode(true);
+    const tableElements = cloneDom.querySelectorAll('table');
+    for (let currentTable of tableElements) {
+      currentTable.style.width = '100%';
+      // currentTable.style.userSelect = 'none';
+      const tdElements = currentTable.querySelectorAll('td')
+      for (let currentTd of tdElements) {
+        currentTd.style.width = '';
+        currentTd.width = '';
+        currentTd.style.padding = '5px';
+        currentTd.style.textAlign = 'center';
+
+      }
+      const spanElements = currentTable.querySelectorAll('span')
+      for (let currentSpan of spanElements) {
+        currentSpan.style.display = 'block';
+      }
     }
     return cloneDom;
   }
